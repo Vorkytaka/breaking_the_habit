d0f08567f17df499a0dfe007ea5f0f9ecd716119
Vorkytaka <vorkytaka@gmail.com>
2022-03-26 21:57:29 +0300

    * Color picker * Create habit

--- lib/ui/home/new_habit_screen.dart
+++ lib/ui/home/new_habit_screen.dart
@@ -1,63 +1,116 @@
+import 'package:breaking_the_habit/model/habit.dart';
 import 'package:flutter/material.dart';
 
-class NewHabitScreen extends StatelessWidget {
+import 'color_picker.dart';
+
+class NewHabitScreen extends StatefulWidget {
   const NewHabitScreen({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<NewHabitScreen> createState() => _NewHabitScreenState();
+}
+
+class _NewHabitScreenState extends State<NewHabitScreen> {
+  Color _color = Colors.blue;
+
+  final TextEditingController _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ListView(
-          physics: const ScrollPhysics(),
-          shrinkWrap: true,
-          padding: const EdgeInsets.symmetric(
-            horizontal: 24,
-            vertical: 16,
-          ),
-          children: [
-            Text(
-              'Новая привычка',
-              style: Theme.of(context).textTheme.headline6,
+        child: Form(
+          child: ListView(
+            physics: const ScrollPhysics(),
+            shrinkWrap: true,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24,
+              vertical: 16,
             ),
-            const SizedBox(height: 32),
-            Row(
-              children: [
-                const Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      label: Text('Название'),
-                    ),
-                    maxLines: 1,
-                    autofocus: true,
-                  ),
-                ),
-                const SizedBox(width: 16),
-                InkResponse(
-                  onTap: () {},
-                  child: Container(
-                    width: 48,
-                    height: 48,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.blue,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            SizedBox(
-              height: 60,
-              child: ElevatedButton.icon(
-                onPressed: () {},
-                icon: Icon(Icons.add),
-                label: Text('Добавить'),
+            children: [
+              Text(
+                'Новая привычка',
+                style: Theme.of(context).textTheme.headline6,
               ),
-            ),
-          ],
+              const SizedBox(height: 32),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _titleController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        label: Text('Название'),
+                      ),
+                      maxLines: 1,
+                      autofocus: true,
+                      validator: (str) {
+                        if (str == null || str.isEmpty) {
+                          return 'Заполните название';
+                        }
+
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  InkResponse(
+                    onTap: () async {
+                      final Color? selectedColor = await showModalBottomSheet(
+                        context: context,
+                        isScrollControlled: true,
+                        builder: (context) => const ColorPickerScreen(),
+                      );
+
+                      if (selectedColor != null) {
+                        setState(() {
+                          _color = selectedColor;
+                        });
+                      }
+                    },
+                    child: Container(
+                      width: 48,
+                      height: 48,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: _color,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Builder(
+                builder: (context) => SizedBox(
+                  height: 60,
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      final form = Form.of(context)!;
+                      if (form.validate()) {
+                        // form.save();
+                        final color = _color;
+                        final title = _titleController.text;
+
+                        final habit = Habit(color: color, title: title);
+                        // todo: save
+                        Navigator.of(context).pop();
+                      }
+                    },
+                    icon: const Icon(Icons.add),
+                    label: const Text('Добавить'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
