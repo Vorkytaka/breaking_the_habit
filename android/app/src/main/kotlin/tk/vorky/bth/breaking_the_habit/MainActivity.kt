742e55bb1b76d8cd2e1975d484b8aa2c907badde
Vorkytaka <vorkytaka@gmail.com>
2022-03-26 20:06:56 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/tk/vorky/bth/breaking_the_habit/MainActivity.kt
@@ -0,0 +1,6 @@
+package tk.vorky.bth.breaking_the_habit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:breaking_the_habit/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
