ede4df053df2e410cfbcfc2e0807d697fd4f2ee9
Vorkytaka <vorkytaka@gmail.com>
2022-03-28 22:55:23 +0300

    * Close screen on delete habit

--- lib/ui/habit/habit_screen.dart
+++ lib/ui/habit/habit_screen.dart
@@ -17,47 +17,63 @@ class HabitScreen extends StatelessWidget {
     final luminance = habit.value.color.computeLuminance();
     final isLight = luminance > 0.5;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(habit.value.title),
-        backgroundColor: habit.value.color,
-        titleTextStyle: Theme.of(context).textTheme.headline6?.copyWith(
-              color: isLight ? Colors.black : Colors.white,
-            ),
-        iconTheme: Theme.of(context).iconTheme.copyWith(
-              color: isLight ? Colors.black : Colors.white,
-            ),
-        actions: [
-          IconButton(
-            onPressed: () async {
-              final bool accepted = (await showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                      title: const Text("Удаление привычки"),
-                      content: Text("Действительно удалить привычку ${habit.value.title}?"),
-                      actions: [
-                        TextButton(
-                          onPressed: () => Navigator.of(context).pop(false),
-                          child: const Text('Нет'),
-                        ),
-                        TextButton(
-                          onPressed: () => Navigator.of(context).pop(true),
-                          child: const Text('Да'),
-                          style: TextButton.styleFrom(primary: Theme.of(context).errorColor),
-                        ),
-                      ],
-                    ),
-                  )) ??
-                  false;
+    return BlocListener<HabitListBloc, HabitListState>(
+      listener: (context, state) {
+        if (state.habits.firstWhereOrNull((element) => element.id == habitId) == null) {
+          Navigator.of(context).pop();
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(habit.value.title),
+          backgroundColor: habit.value.color,
+          titleTextStyle: Theme.of(context).textTheme.headline6?.copyWith(
+                color: isLight ? Colors.black : Colors.white,
+              ),
+          iconTheme: Theme.of(context).iconTheme.copyWith(
+                color: isLight ? Colors.black : Colors.white,
+              ),
+          actions: [
+            IconButton(
+              onPressed: () async {
+                final bool accepted = (await showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: const Text("Удаление привычки"),
+                        content: Text("Действительно удалить привычку ${habit.value.title}?"),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.of(context).pop(false),
+                            child: const Text('Нет'),
+                          ),
+                          TextButton(
+                            onPressed: () => Navigator.of(context).pop(true),
+                            child: const Text('Да'),
+                            style: TextButton.styleFrom(primary: Theme.of(context).errorColor),
+                          ),
+                        ],
+                      ),
+                    )) ??
+                    false;
 
-              if (accepted) {
-                context.read<Repository>().delete(habit.id);
-              }
-            },
-            icon: const Icon(Icons.delete),
-          ),
-        ],
+                if (accepted) {
+                  context.read<Repository>().delete(habit.id);
+                }
+              },
+              icon: const Icon(Icons.delete),
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
+
+extension ListUtils<E> on List<E> {
+  E? firstWhereOrNull(bool Function(E element) test) {
+    for (E element in this) {
+      if (test(element)) return element;
+    }
+    return null;
   }
 }
