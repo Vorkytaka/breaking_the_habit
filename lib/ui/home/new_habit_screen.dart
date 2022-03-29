acfdc59b23fd465df095bc46f353dd35c8682754
Vorkytaka <vorkytaka@gmail.com>
2022-03-29 22:43:17 +0300

    * Remove old color picker

--- lib/ui/home/new_habit_screen.dart
+++ lib/ui/home/new_habit_screen.dart
@@ -4,8 +4,6 @@ import 'package:breaking_the_habit/model/habit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'color_picker.dart';
-
 class NewHabitScreen extends StatefulWidget {
   const NewHabitScreen({
     Key? key,
