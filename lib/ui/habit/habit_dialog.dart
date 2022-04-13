db5f3e4352772891ac0434290a993c2d5360b3fd
Vorkytaka <vorkytaka@gmail.com>
2022-04-13 22:54:27 +0300

    * Edit habit

--- lib/ui/habit/habit_dialog.dart
+++ lib/ui/habit/habit_dialog.dart
@@ -1,5 +1,8 @@
 import 'package:breaking_the_habit/bloc/habit/habit_list_bloc.dart';
+import 'package:breaking_the_habit/color_dialog.dart';
 import 'package:breaking_the_habit/data/repository.dart';
+import 'package:breaking_the_habit/model/habit.dart';
+import 'package:breaking_the_habit/model/id_model.dart';
 import 'package:breaking_the_habit/utils/collections.dart';
 import 'package:breaking_the_habit/utils/colors.dart';
 import 'package:flutter/material.dart';
@@ -59,12 +62,15 @@ class HabitDialog extends StatelessWidget {
                     horizontal: 16,
                   ),
                   title: Text(habit.value.title),
-                  leading: Container(
-                    width: 32,
-                    height: 32,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: habit.value.color,
+                  leading: IconButton(
+                    onPressed: null,
+                    icon: Container(
+                      width: 35,
+                      height: 35,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: habit.value.color,
+                      ),
                     ),
                   ),
                   trailing: IconTheme.merge(
@@ -76,7 +82,9 @@ class HabitDialog extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
                         IconButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            _showEditHabitDialog(context: context, habit: habit);
+                          },
                           icon: const Icon(Icons.edit),
                         ),
                         IconButton(
@@ -112,6 +120,118 @@ class HabitDialog extends StatelessWidget {
                   ),
                 );
               },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Future<void> _showEditHabitDialog({
+  required BuildContext context,
+  required IDModel<Habit> habit,
+}) {
+  return showDialog(
+    context: context,
+    barrierColor: Colors.transparent,
+    barrierDismissible: true,
+    builder: (context) => _EditHabitDialog(habit: habit),
+  );
+}
+
+class _EditHabitDialog extends StatefulWidget {
+  final IDModel<Habit> habit;
+
+  const _EditHabitDialog({Key? key, required this.habit}) : super(key: key);
+
+  @override
+  State<_EditHabitDialog> createState() => _EditHabitDialogState();
+}
+
+class _EditHabitDialogState extends State<_EditHabitDialog> {
+  late final TextEditingController _titleController;
+  late Color _color;
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController = TextEditingController(text: widget.habit.value.title);
+    _color = widget.habit.value.color;
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: MediaQuery.of(context).viewInsets +
+          const EdgeInsets.symmetric(
+            horizontal: 8,
+            vertical: 16,
+          ),
+      child: MediaQuery.removeViewInsets(
+        removeTop: true,
+        removeRight: true,
+        removeBottom: true,
+        removeLeft: true,
+        context: context,
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Material(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            clipBehavior: Clip.hardEdge,
+            child: ListTile(
+              tileColor: Theme.of(context).colorScheme.surface,
+              contentPadding: const EdgeInsets.symmetric(
+                vertical: 8,
+                horizontal: 16,
+              ),
+              title: TextFormField(
+                controller: _titleController,
+                decoration: const InputDecoration(border: InputBorder.none),
+                autofocus: true,
+              ),
+              leading: CloseButton(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+              trailing: IconTheme.merge(
+                data: IconThemeData(color: Theme.of(context).colorScheme.onSurface),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    ColorPickerButton(
+                      currentColor: _color,
+                      onSelected: (color) {
+                        _color = color;
+                        setState(() {});
+                      },
+                    ),
+                    IconButton(
+                      onPressed: () async {
+                        final title = _titleController.text;
+                        final color = _color;
+                        final newHabit = IDModel(
+                          id: widget.habit.id,
+                          value: Habit(
+                            color: color,
+                            title: title,
+                          ),
+                        );
+
+                        await context.read<Repository>().updateHabit(newHabit);
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(Icons.done),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         ),
